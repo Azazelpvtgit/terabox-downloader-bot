c7fa92b2e3256092d4161b3fd2645cf290712058
Azazelpvtgit <azazelfl4me@gmail.com>
2025-01-24 16:37:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 21505404  # api id
+API_HASH = "5feffdf4111ed339381056d9476d3fcd"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "7950099303:AAG5HbTFqP1Xl0_ao6aTPSVZijfCBgwKEOo"  # bot token
 
 
 # REDIS
-HOST = "localhost"  # redis host uri
-PORT = 6379  # redis port
-PASSWORD = ""  # redis password
+HOST = "redis-19496.crce179.ap-south-1-1.ec2.redns.redis-cloud.com:"  # redis host uri
+PORT = 19496  # redis port
+PASSWORD = "9xRYTkCMjdqjWK3Zj7EeQQwLrNA3iSuV"  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1001512869496  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = ""
-ADMINS = [1317173146]
+COOKIE = "cookies.txt"
+ADMINS = [7804819854]
 
 
-BOT_USERNAME = "teraboxdown_bot"
+BOT_USERNAME = "Terabox_video_downloader_Fbot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = "@Fl4me_Updates"
 
 PUBLIC_EARN_API = ""  # https://publicearn.com/api
